13ad47eb337889fcb14791a92a1b6814b6cc24aa
JiaxuYan <970440700@qq.com>
2018-07-01 23:55:15 +0800

    origin

---
+++ src/main/java/com/huawei/hicloud/GitApplication.java
@@ -0,0 +1,18 @@
+package com.huawei.hicloud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(GitApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("### GitApplication ...");
+		SpringApplication.run(GitApplication.class, args);
+	}
+
+}
